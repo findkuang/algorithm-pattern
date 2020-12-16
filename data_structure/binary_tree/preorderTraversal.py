2edd8d6a6b800fed02f81610ff48c55bbba87924
findkuang <findkuang@163.com>
2020-12-16 23:16:57 +0800

    20201216-02

--- data_structure/binary_tree/preorderTraversal.py
+++ data_structure/binary_tree/preorderTraversal.py
@@ -3,8 +3,9 @@
 # @Time    : 2020/12/16 18:13
 # @Author  : kuangxiaojiang
 # @File    : preorderTraversal.py
-# @Function: 前序非递归
+# @Function: 二叉树前序非递归、中序非递归、后续非递归遍历
 import collections
+
 
 class Solution:
     def preorderTraversal(self, root):
@@ -13,10 +14,32 @@ class Solution:
         if root is None:
             return rst_list
         stack.append(root)
-        node = stack[:-1]
         while len(stack) > 0:
-            if not node.left:
-                stack.append(node.left)
+            node = stack.pop()
+            rst_list.append(node.value)
             if not node.right:
                 stack.append(node.right)
+            if not node.left:
+                stack.append(node.left)
+        return rst_list
+
+    def inorderTraversal(self, root):
+        stack = []
+        rst_list = []
+        if root is None:
+            return rst_list
+        stack.append(root)
+        while len(stack) > 0:
+            node = stack.pop()
+            if node.right or node.left:
+                if not node.right:
+                    stack.append(node.right)
+                stack.append(node.value)
+                if not node.left:
+                    stack.append(node.left)
+            else:
+                rst_list.append(node.value)
+
+    def postorderTraversal(self, root):
+        pass
 
