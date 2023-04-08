6b16595bf54bdfbba36218cbc01586885d016ce6
findkuang <findkuang@163.com>
2023-04-08 17:15:06 +0800

    test

--- Introduction/test.py
+++ Introduction/test.py
@@ -2,4 +2,21 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/2/20 0020 16:06
 # @Author  : Aisonk
-# @Function    : 
+# @Function    :
+
+
+def num_is_palindrome(num: int):
+    if 10000 <= num < 100000:
+        num = str(num)
+        new_num = num[::-1]
+        if num == new_num:
+            return True
+        else:
+            return False
+    else:
+        message = '输入数据错误,请重新输入'
+        return message
+
+
+a = 12321
+print(num_is_palindrome(a))
--- Introduction/test01.py
+++ Introduction/test01.py
@@ -2,4 +2,24 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/4/4 0004 23:28
 # @Author  : Aisonk
-# @Function    : 
+# @Function    :
+# !encoding=utf8
+def ip_add_func(ip: str):
+    ip_list = ip.split('.')
+    for i in range(len(ip_list) - 1, -1, -1):
+        if int(ip_list[i]) < 255:
+            ip_list[i] = str(int(ip_list[i]) + 1)
+            return '.'.join(ip_list)
+        else:
+            ip_list[i] = '0'
+    return "非法ip"
+
+
+if __name__ == '__main__':
+    # ip = '0.0.0.0'
+    ip = '0.0.1.251'
+    # ip = '255.255.255.0'
+    # ip = '255.255.255.254'
+    # ip = '10.10.0.254'
+    # ip = '255.255.255.255'
+    print(ip_add_func(ip))
